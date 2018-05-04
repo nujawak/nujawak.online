fbdb88544b60c419c8a0d5911b3c07d0fa212a46
nujawak <nujawak.online@gmail.com>
2018-05-04 17:19:38 +0900

    update: page style

--- dest/page.php
+++ dest/page.php
@@ -9,4 +9,38 @@
 ?>
 <?php get_header(); ?>
 
+<?php while ( have_posts() ) : the_post(); ?>
+	
+	<main id="post-<?php the_ID(); ?>" <?php post_class('p-page'); ?>>
+		<article class="p-page__panel">
+
+			<?php if ( get_the_post_thumbnail() ) :?>
+				<figure class="p-page__thumb">
+					<?php the_post_thumbnail(); ?>
+				</figure>
+			<?php endif; ?>
+
+			<header class="p-page__header">
+				<h1 class="p-page__title"><?php the_title(); ?></h1>
+			</header>
+
+			<div class="p-page__content">
+				<?php the_content(); ?>
+			</div>
+
+			<footer class="p-page__footer">
+			</footer>
+			
+		</article>
+		
+		<nav class="c-breadcrumb">
+			<ol class="c-breadcrumb__list">
+				<li class="c-breadcrumb__item"><a href="/">home</a></li>
+				<li class="c-breadcrumb__item"><?php the_title(); ?></li>
+			</ol>
+		</nav><!-- m-breadcrumb -->
+	</main>
+	
+<?php endwhile; ?>
+
 <?php get_footer(); ?>
