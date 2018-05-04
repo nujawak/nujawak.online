7038354adde4f3f0f59a1bf8f404081635401e93
nujawak <nujawak.online@gmail.com>
2018-05-04 14:59:31 +0900

    update: single style

--- dest/single.php
+++ dest/single.php
@@ -9,4 +9,45 @@
 ?>
 <?php get_header(); ?>
 
+<?php while ( have_posts() ) : the_post(); ?>
+	
+	<main id="post-<?php the_ID(); ?>" <?php post_class('p-single'); ?>>
+		<article class="p-single__panel">
+
+			<?php if ( get_the_post_thumbnail() ) :?>
+				<figure class="p-single__thumb">
+					<?php the_post_thumbnail(); ?>
+				</figure>
+			<?php endif; ?>
+
+			<header class="p-single__header">
+				<h1 class="p-single__title"><?php the_title(); ?></h1>
+				<div class="p-single__meta">
+					<time class="p-single__time" datetime="<?php the_time( 'Y-m-d' ); ?>"><?php the_time( 'Y.m.d' ); ?></time>
+					<p class="p-single__categories"><?php the_category( ', ' ); ?></p>
+				</div><!-- p-single__meta -->
+			</header>
+
+			<div class="p-single__content">
+				<?php the_content(); ?>
+			</div>
+
+			<footer class="p-single__footer">
+				
+			</footer>
+			
+		</article>
+		
+		<nav class="c-breadcrumb">
+			<ol class="c-breadcrumb__list">
+				<li class="c-breadcrumb__item"><a href="/">home</a></li>
+				<li class="c-breadcrumb__item"><a href="/blog/">blog</a></li>
+				<li class="c-breadcrumb__item"><?php the_category( ', ' ); ?></li>
+				<li class="c-breadcrumb__item"><?php the_title(); ?></li>
+			</ol>
+		</nav><!-- m-breadcrumb -->
+	</main>
+	
+<?php endwhile; ?>
+ 
 <?php get_footer(); ?>
